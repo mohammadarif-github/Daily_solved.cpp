7a23e1bed5a3f5df25ac5d5fc5af121af46e0a97
Mohammad Ariful Islam <74112009+mohammadarif-github@users.noreply.github.com>
2023-08-07 23:49:49 +0600

    Add files via upload

---
+++ Week 9/Day 4/A_Square_String.cpp
@@ -0,0 +1,40 @@
+/*
+
+    Author :--
+    !~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!
+    |                                         |
+    |                                         |
+    |  cout<< "MOHAMMAD ARIFUL ISLAM"<< endl  |
+    |                                         |
+    |                                         |
+    !~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!
+
+*/
+#include <bits/stdc++.h>
+#define boost                     \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(NULL);
+#define ll long long
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define minus cout << -1 << endl;
+
+using namespace std;
+int main()
+{
+    boost;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        if(s.size()%2)no;
+        else{
+            if(s.substr(0,s.size()/2)== s.substr(s.size()/2,s.size()))yes;
+            // cout << s.substr(0,s.size()/2) << endl;
+            else no;
+        }
+    }
+    
+}
