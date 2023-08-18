c948e1c17bc5fbd39d02f7f02430c90491ab9a12
Mohammad Ariful Islam <74112009+mohammadarif-github@users.noreply.github.com>
2023-08-18 17:39:18 +0600

    Add files via upload

---
+++ Week 10/Day 7/A_Nezzar_and_Colorful_Balls.cpp
@@ -0,0 +1,44 @@
+/*
+
+    Author :--
+    !~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!
+    |                                         |
+    |                                         |
+    |  cout<< "MOHAMMAD ARIFUL ISLAM"<< endl  |
+    |                                         |
+    |                                         |
+    !~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!
+
+*/
+#include <bits/stdc++.h>
+#define boost                     \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(NULL);
+#define ll long long
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define minus cout << -1 << endl;
+
+using namespace std;
+int main()
+{
+    boost;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        map<int,int>mp;
+        int maxx = INT_MIN;
+        for(int i =0 ;i<n;i++)
+        {
+            int in;
+            cin >> in;
+            mp[in]++;
+            maxx = max(maxx,mp[in]);
+        }
+        cout << maxx << endl;
+    }
+    
+}
